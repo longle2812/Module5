1d948d4242e9b66635b6187d46c426a715c6746b
longle2812 <lehailong2812@gmail.com>
2021-07-15 15:32:03 +0700

    [Thực hành] Khai báo và sử dụng biến

---
+++ demo/src/main.js
@@ -0,0 +1,9 @@
+var arrow = function () {
+    var width;
+    var height;
+    width = 10.5;
+    height = 20;
+    var area = width * height;
+    console.log("Di\u1EC7n t\u00EDch h\u00ECnh ch\u1EEF nh\u1EADt: " + area);
+};
+arrow();
